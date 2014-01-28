464c99308027d43b0c01514eb184345f2c58ae1e
martha esdrelon <tmmesdrelon@gmail.com>
2014-01-28 15:56:52 +0800

    Created a rails application(Home) and added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Home::Application.config.secret_key_base = '7e481c1c1f9e91ee4fac4f8b83c9e75308a47931a6d46ad2db2fd9551019fff7531062d355523caccb9436218f0ad94039b6eef9cfeda51d94111c11c6a68f20'
